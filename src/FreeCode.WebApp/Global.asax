49004132bc5e349aa3b4b4770e4081aa6698a239
Dan Gilleland <dgilleland@nait.ca>
2019-09-23 11:24:02 -0600

    Web App with MessageUserControl

---
+++ src/FreeCode.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FreeCode.WebApp.Global" Language="C#" %>
---
+++ src/FreeCode.WebApp/UserControls/ProcessRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FreeCode.WebApp.UserControls
+{
+    public delegate void ProcessRequest();
+}
